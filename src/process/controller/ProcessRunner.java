054fb74ae82f6a0d420314257482f462ba7aafa9
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-07 12:25:05 -0600

    added thumbs.db to .gitignore and added text to classes

--- src/process/controller/ProcessRunner.java
+++ src/process/controller/ProcessRunner.java
@@ -1,6 +1,10 @@
-package process.controller;
+ package process.controller;
 
 public class ProcessRunner
 {
-
+	public static void main( String [] args)
+	{
+		ProcessController appController =  new ProcessController();
+		appController.start();
+	}
 }
