d75b60a6302718b416d1094499f8e71864e1f8ed
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-10-25 12:36:17 -0600

    Add an array section from when we were practicing using and creating Arrays.

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -1,6 +1,9 @@
 package process.controller;
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 import process.model.PlayDohCircle;		//gets the package and the class from elsewhere so you can use it here
 import process.model.PlayDohSnek;
 
@@ -9,10 +12,48 @@ import process.model.PlayDohSnek;
  * @author ksin8488
  *
  */
+
 public class ProcessController
 {
+	private String [] hats = {"One", "Two", "Three"};		//can initialize in the decleration
+	private int [] numbers;
+	
+	public ProcessController()
+	{
+		hats = new String[5];
+		numbers = new int [10];
+	}
+	
 	public void start()
 	{
+		//ARRAY PRACTICE 10/25/17
+		System.out.println(hats);
+		System.out.println(numbers);
+		
+		hats[0] = "Benie";
+		hats[1] = "Fedora";
+		hats[2] = "Beret";
+		hats[4] = "Cap";		//order of how you put them doesn't matter
+		hats[3] = "Balaclava";
+		
+		System.out.println(hats[0]);
+		System.out.println(hats[1]);
+		System.out.println(hats[3]);
+		
+		for (String current : hats)
+		{
+			System.out.println(current);
+		}
+		
+		for(int index = 0; index < numbers.length; index += 1)		//the last index is 9 so you can't do index <= numbers.length; You also can't to negatives
+		{
+			numbers[index] = index +1;
+			
+			System.out.println(numbers[index]);
+		}
+		
+		hats = new String [] {"one", "two", "three", "four", "five"};		//can re-initialize arrays. Only works with arrays
+		
 		System.out.println("This is a practice project.");
 		System.out.println("By practicing I develop muscle memory :D ");
 		
@@ -46,5 +87,8 @@ public class ProcessController
 		System.out.println("Here are the sneks I made! They're not scary. It even has a hat! ");
 		System.out.println(snekOne);
 		System.out.println(snekTwo);
+		
+		//ARRAY PRACTICE
+		
 	}
 }
